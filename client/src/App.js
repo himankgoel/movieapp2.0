97294c6a54505621b7d132e0a2142670ce02572f
himankgoel <himankgoel12@gmail.com>
2021-01-06 12:55:25 +0530

    Patched

--- client/src/App.js
+++ client/src/App.js
@@ -1,5 +1,5 @@
 import React, { useState, useEffect } from "react";
-import { BrowserRouter, Route} from "react-router-dom";
+import { BrowserRouter, Route } from "react-router-dom";
 import Home from "./Components/Home";
 import Login from "./Components/Login";
 import Register from "./Components/Register";
@@ -13,7 +13,7 @@ import NavbarUserLoggedIn from "./Components/NavbarUserLoggedIn";
 function App() {
   const [user, setUser] = useState(null);
 
-  async function tokenVerify () {
+  async function tokenVerify() {
     if (localStorage.getItem("token") != null) {
       const resp = await toVerifyToken();
       if (resp.status === 200) setUser(resp.data.user1);
@@ -28,16 +28,16 @@ function App() {
   }, [user]);
   return (
     <BrowserRouter>
-        <div>
-          <UserContext.Provider value={{ user, setUser }}>
-            {user ? <NavbarUserLoggedIn /> : <Navbar />}
-            <Route exact path="/login" component={Login} />
-            <Route exact path="/" component={Home} />
-            <Route exact path="/register" component={Register} />
-            <Route exact path="/info/:movieid" component={MovieDetailPage} />
-            <Route exact path="/favourite-movies" component={FavouriteMovies} />
-          </UserContext.Provider>
-        </div>
+      <div>
+        <UserContext.Provider value={{ user, setUser }}>
+          {user ? <NavbarUserLoggedIn /> : <Navbar />}
+          <Route exact path="/login" component={Login} />
+          <Route exact path="/" component={Home} />
+          <Route exact path="/register" component={Register} />
+          <Route exact path="/info/:movieid" component={MovieDetailPage} />
+          <Route exact path="/favourite-movies" component={FavouriteMovies} />
+        </UserContext.Provider>
+      </div>
     </BrowserRouter>
   );
 }
--- server.js
+++ server.js
@@ -1,4 +1,4 @@
-require("dotenv").config();                 //FOR SAVING SECRET AND KEYS
+require("dotenv").config(); //FOR SAVING SECRET AND KEYS
 const express = require("express");
 const bodyParser = require("body-parser");
 const mongoose = require("mongoose");
@@ -6,38 +6,42 @@ const bcrypt = require("bcrypt");
 const jwt = require("jsonwebtoken");
 const path = require("path");
 
-
 const app = express();
 
-app.use(bodyParser.urlencoded({ extended: true })); 
+app.use(bodyParser.urlencoded({ extended: true }));
 app.use(bodyParser.json());
-mongoose.set('useCreateIndex', true);
+mongoose.set("useCreateIndex", true);
 
-const port = process.env.PORT || 5000 ;
- 
+const port = process.env.PORT || 5000;
+
 //CONNECTING TO MONGODB
-mongoose.connect(`mongodb+srv://admin:${process.env.MONGO_PASSWORD}@cluster0.r2ehn.mongodb.net/<dbname>?retryWrites=true&w=majority` ,{ useNewUrlParser: true ,  useUnifiedTopology: true })
-.then(() => console.log("MongoDB connected"))
-.catch((err) => {console.log(err)});
-//movieUser Schema with Name, Email, Password as Hash 
+const URI = `mongodb+srv://user:s3oTPRp8I89vZ6qC@cluster0.r2ehn.mongodb.net/User?retryWrites=true&w=majority`;
+mongoose
+  .connect(URI, {
+    useNewUrlParser: true,
+    useCreateIndex: true,
+    useUnifiedTopology: true,
+  })
+  .then(() => console.log("MongoDB Connected..."))
+  .catch((err) => console.log(err));
+//movieUser Schema with Name, Email, Password as Hash
 
 const movieDBUserSchema = new mongoose.Schema({
-    name: {
-        type: String,
-        required: true
-    },
-    email:{
-        type:String,
-        required:true
-    },
-    password:{
-        type:String,
-        required:true
-    },
-    favouriteMovies: [], //ARRAY OF OBJECTS 
+  name: {
+    type: String,
+    required: true,
+  },
+  email: {
+    type: String,
+    required: true,
+  },
+  password: {
+    type: String,
+    required: true,
+  },
+  favouriteMovies: [], //ARRAY OF OBJECTS
 });
 const movieUser = mongoose.model("movieUser", movieDBUserSchema);
-
 
 //ROUTES
 
@@ -49,134 +53,149 @@ const movieUser = mongoose.model("movieUser", movieDBUserSchema);
 //     .then(user => res.json(user));
 //  });
 
-
-
 // @route POST
 //@desc  Save new movieUser (Register)
 //@Access Public
- app.post("/api/user/register" , (req,res) => {
-     //Checking for empty fields
-     const {name , email , password} = req.body;
-     if(!name || !email || !password){
-        return res.status(204).json({msg:"Please enter all the fields!"})
-    }
-    //Checking for existing user
-    movieUser.findOne({email})
-    .then(user => {
-        if(user)
-        return res.status(400).json({msg: "Entered email id already exists. Please Login instead."});
-       })
-       .catch(err => {
-           console.log(err);
-       });
-        //Create salt and Hash
-        bcrypt.hash(req.body.password,10,(err,hash) => {
-            if(err) throw err;
-            const newUser = new movieUser({ 
-                name: req.body.name,
-                email : req.body.email,
-                password: hash
-                });
-                newUser.save()
-                .then(user => {
-                    jwt.sign(
-                        {id: user.id },process.env.JWTSECRET,{expiresIn:3600},(err,token) => {
-                            if(err) throw err;
-                            res.json({token , msg : "Registered Successfully!"});
-                        }
-                    )
-                });    
-        }); 
+app.post("/api/user/register", (req, res) => {
+  //Checking for empty fields
+  const { name, email, password } = req.body;
+  if (!name || !email || !password) {
+    return res.status(204).json({ msg: "Please enter all the fields!" });
+  }
+  //Checking for existing user
+  movieUser
+    .findOne({ email })
+    .then((user) => {
+      if (user)
+        return res.status(400).json({
+          msg: "Entered email id already exists. Please Login instead.",
+        });
+    })
+    .catch((err) => {
+      console.log(err);
     });
+  //Create salt and Hash
+  bcrypt.hash(req.body.password, 10, (err, hash) => {
+    if (err) throw err;
+    const newUser = new movieUser({
+      name: req.body.name,
+      email: req.body.email,
+      password: hash,
+    });
+    newUser.save().then((user) => {
+      jwt.sign(
+        { id: user.id },
+        process.env.JWTSECRET,
+        { expiresIn: 3600 },
+        (err, token) => {
+          if (err) throw err;
+          res.json({ token, msg: "Registered Successfully!" });
+        }
+      );
+    });
+  });
+});
 // @route POST
 //@desc Fetch data of a user(LOGIN)
-//@Access Public 
-app.post("/api/user/login" , (req,res) => {
-
-    //Checking for empty fields
-    const { email , password} = req.body;
-    if(!email || !password){
-       return res.status(400).json({msg:"Please enter all the fields!"})
-   }
-   //Checking for existing user
-   movieUser.findOne({email})
-   .then(user => {
-       if(!user)
-       return res.status(404).json({msg: "Email doesn't exists. Register instead?"});
-       //Validate passwords
-       bcrypt.compare(password,user.password,(err,result) => {
-        if(!result) return res.status(400).json({msg:"Invalid Credentials"}); 
-        jwt.sign(
-            {id: user.id },process.env.JWTSECRET,{expiresIn:3600},(err,token) => {
-                if(err) throw err;
-                return res.status(200).json({token, msg : "Login Successful !"});
-            }
-        )
-        
-        });
-      });
-    });
-
-    // @route POST
-    //@desc VERIFYING JWT 
-
-    app.post("/api/tokenverify",(req,res) => {
-        try {
-            var decoded = jwt.verify(req.body.token , process.env.JWTSECRET);
-            movieUser.findById(decoded.id,(err,user) => {
-                if(!err){
-                const user1 = {
-                    name : user.name,
-                    favouriteMovies : user.favouriteMovies,
-                    userLoggedIn : true
-                };
-                res.status(200).json({user1});
-                }
-            });
-          } catch(err) {
-            // err
-            res.status(400).json({msg:"Invalid Data"});
-          }
-        
-    });
-
-    // @ROUTE POST
-    //desc PUSH THE Favourite Movie TO THE DATABASE 
-    app.post("/api/user/addmovie", (req, res) => {
-        try {var decoded = jwt.verify(req.body.token , process.env.JWTSECRET);
-            if(req.body.imdbID === null) return;
-        movieUser.findByIdAndUpdate(decoded.id, {$push: {favouriteMovies: req.body.imdbID}},{useFindAndModify:false},(err,res) => {
-            if(!err){
-                console.log("added to fav");
-            }
-        });
-        }catch(err){ 
-           console.log("bad request");
+//@Access Public
+app.post("/api/user/login", (req, res) => {
+  //Checking for empty fields
+  const { email, password } = req.body;
+  if (!email || !password) {
+    return res.status(400).json({ msg: "Please enter all the fields!" });
+  }
+  //Checking for existing user
+  movieUser.findOne({ email }).then((user) => {
+    if (!user)
+      return res
+        .status(404)
+        .json({ msg: "Email doesn't exists. Register instead?" });
+    //Validate passwords
+    bcrypt.compare(password, user.password, (err, result) => {
+      if (!result) return res.status(400).json({ msg: "Invalid Credentials" });
+      jwt.sign(
+        { id: user.id },
+        process.env.JWTSECRET,
+        { expiresIn: 3600 },
+        (err, token) => {
+          if (err) throw err;
+          return res.status(200).json({ token, msg: "Login Successful !" });
         }
-     });
-     
-     // @route POST
-     //desc DELETE THE PARTICULAR NOTE FROM DATABASE BY NOTEID 
-      app.post("/api/user/deletemovie", (req, res) => {
-        try {var decoded = jwt.verify(req.body.token , process.env.JWTSECRET);
-            if(req.body.imdbID === null) return;
-        movieUser.findByIdAndUpdate(decoded.id, {$pull: {favouriteMovies:req.body.imdbID }},{useFindAndModify:false},(err,res) => {
-            if(!err){
-                console.log("removed from fav");
-            }
-        });
-        }catch(err){ 
-           console.log("bad request");
+      );
+    });
+  });
+});
+
+// @route POST
+//@desc VERIFYING JWT
+
+app.post("/api/tokenverify", (req, res) => {
+  try {
+    var decoded = jwt.verify(req.body.token, process.env.JWTSECRET);
+    movieUser.findById(decoded.id, (err, user) => {
+      if (!err) {
+        const user1 = {
+          name: user.name,
+          favouriteMovies: user.favouriteMovies,
+          userLoggedIn: true,
+        };
+        res.status(200).json({ user1 });
+      }
+    });
+  } catch (err) {
+    // err
+    res.status(400).json({ msg: "Invalid Data" });
+  }
+});
+
+// @ROUTE POST
+//desc PUSH THE Favourite Movie TO THE DATABASE
+app.post("/api/user/addmovie", (req, res) => {
+  try {
+    var decoded = jwt.verify(req.body.token, process.env.JWTSECRET);
+    if (req.body.imdbID === null) return;
+    movieUser.findByIdAndUpdate(
+      decoded.id,
+      { $push: { favouriteMovies: req.body.imdbID } },
+      { useFindAndModify: false },
+      (err, res) => {
+        if (!err) {
+          console.log("added to fav");
         }
-     });
-    
-    
+      }
+    );
+  } catch (err) {
+    console.log("bad request");
+  }
+});
+
+// @route POST
+//desc DELETE THE PARTICULAR NOTE FROM DATABASE BY NOTEID
+app.post("/api/user/deletemovie", (req, res) => {
+  try {
+    var decoded = jwt.verify(req.body.token, process.env.JWTSECRET);
+    if (req.body.imdbID === null) return;
+    movieUser.findByIdAndUpdate(
+      decoded.id,
+      { $pull: { favouriteMovies: req.body.imdbID } },
+      { useFindAndModify: false },
+      (err, res) => {
+        if (!err) {
+          console.log("removed from fav");
+        }
+      }
+    );
+  } catch (err) {
+    console.log("bad request");
+  }
+});
+
 // if(process.env.NODE_ENV === "production"){
 //     app.use(express.static("client/build"));
 //     app.get("*",(req,res) => {
 //         res.sendFile(path.resolve(__dirname,"client","build","index.html"));
 //     });
 // }
-app.listen(port,() => {
-    console.log("Server started at port 5000!");
+app.listen(port, () => {
+  console.log("Server started at port 5000!");
 });
